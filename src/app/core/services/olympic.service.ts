68b58dc2f96060d741f0475fc2d3b9317b2e588d
Tom ANDRE <tom.andre@soprasteria.com>
2024-10-06 19:19:14 +0200

    Création des models et modification du service avec gestion des erreurs

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,31 +1,38 @@
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import {BehaviorSubject, retry, throwError} from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import {Olympic} from "../models/Olympic";
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
-  private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympicUrl = './assets/mock/olympic.json'
+  private olympics$ = new BehaviorSubject<Olympic[]>([])
 
   constructor(private http: HttpClient) {}
-
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
-      catchError((error, caught) => {
-        // TODO: improve error handling
-        console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
-        return caught;
-      })
+      retry(5),
+      catchError(this.handleError)
     );
   }
 
   getOlympics() {
     return this.olympics$.asObservable();
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('An error occured', error.error)
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, body was:`,
+        error.error
+      )
+    }
+    return throwError(() => new Error('Something bad happend: please try again later.'))
   }
 }
