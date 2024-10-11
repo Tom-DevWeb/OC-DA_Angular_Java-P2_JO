e3a8bba75132f60277e3c0b071f16268bd398ad6
Tom ANDRE <tom.andre@soprasteria.com>
2024-10-11 17:57:40 +0200

    feat: Modification de la page NotFound404

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
-import {ApplicationConfig} from "@angular/core";
+import {ApplicationConfig, provideZoneChangeDetection} from "@angular/core";
 import {provideRouter} from "@angular/router";
 import {routes} from "./app.routes";
 import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimations()
   ]
 }
--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,9 +1,12 @@
 import {Component, OnInit} from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
